8460949d7dd7aef643c2208b35198affeeffc7d0
asaaev <niaava1@gmail.com>
2023-10-31 19:40:46 +0200

    Add initialization logic and test for upload directory

---
+++ src/main/java/com/grimacegram/grimacegram/configuration/WebConfiguration.java
@@ -0,0 +1,23 @@
+package com.grimacegram.grimacegram.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    CommandLineRunner commandLineRunner(){
+        return (args) -> {
+            File uploadFolder = new File("upload-test");
+            boolean uploadFolderExist = uploadFolder.exists() && uploadFolder.isDirectory();
+            if(!uploadFolderExist){
+                uploadFolder.mkdir();
+            }
+        };
+    }
+}
