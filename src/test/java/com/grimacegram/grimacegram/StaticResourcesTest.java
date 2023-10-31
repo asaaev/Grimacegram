5fb509031aad4badef0bd87374d197eacb5887b4
asaaev <niaava1@gmail.com>
2023-10-31 20:55:31 +0200

    Add initialization logic for multiple upload folders and their corresponding tests

--- src/test/java/com/grimacegram/grimacegram/StaticResourcesTest.java
+++ src/test/java/com/grimacegram/grimacegram/StaticResourcesTest.java
@@ -1,7 +1,10 @@
 package com.grimacegram.grimacegram;
 
+import com.grimacegram.grimacegram.configuration.AppConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,11 +19,28 @@ import java.io.File;
 @ActiveProfiles("test")
 public class StaticResourcesTest {
 
+@Autowired
+    AppConfiguration appConfiguration;
+
     @Test
     public void checkStaticFolder_whenAppIsInitialized_uploadFolderMustExist(){
-        File uploadFolder = new File("upload-test");
+        File uploadFolder = new File(appConfiguration.getUploadPath());
         boolean uploadFolderExist = uploadFolder.exists() && uploadFolder.isDirectory();
         assertThat(uploadFolderExist).isTrue();
+    }
+    @Test
+    public void checkStaticFolder_whenAppIsInitialized_profileImageSubFolderMustExist(){
+        String profileImageFolderPath = appConfiguration.getFullProfileImagesPath();
+        File profileImageFolder = new File(profileImageFolderPath);
+        boolean profileImageFolderExist = profileImageFolder.exists() && profileImageFolder.isDirectory();
+        assertThat(profileImageFolderExist).isTrue();
+    }
+    @Test
+    public void checkStaticFolder_whenAppIsInitialized_attachmentsSubfolderMustExist(){
+        String attachmentsFolderPath = appConfiguration.getFullAttachmentsPath();
+        File attachmentFolder = new File(attachmentsFolderPath);
+        boolean attachmentsFolderExist = attachmentFolder.exists() && attachmentFolder.isDirectory();
+        assertThat(attachmentsFolderExist).isTrue();
     }
 
 }
