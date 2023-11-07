92bd842148a2e95975935907ad05d86036cc4a1a
asaaev <niaava1@gmail.com>
2023-11-07 23:44:22 +0200

    Revert "Add initialization logic for multiple upload folders and their corresponding tests"
    This reverts commit 5fb509031aad4badef0bd87374d197eacb5887b4.

--- src/main/java/com/grimacegram/grimacegram/configuration/WebConfiguration.java
+++ src/main/java/com/grimacegram/grimacegram/configuration/WebConfiguration.java
@@ -1,6 +1,5 @@
 package com.grimacegram.grimacegram.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,23 +10,14 @@ import java.io.File;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Autowired
-    AppConfiguration appConfiguration;
-
     @Bean
     CommandLineRunner commandLineRunner(){
         return (args) -> {
-            createNonExistingFolder(appConfiguration.getUploadPath());
-            createNonExistingFolder(appConfiguration.getFullProfileImagesPath());
-            createNonExistingFolder(appConfiguration.getFullAttachmentsPath());
+            File uploadFolder = new File("upload-test");
+            boolean uploadFolderExist = uploadFolder.exists() && uploadFolder.isDirectory();
+            if(!uploadFolderExist){
+                uploadFolder.mkdir();
+            }
         };
-    }
-
-    private void createNonExistingFolder(String path) {
-        File folder = new File(path);
-        boolean folderExist = folder.exists() && folder.isDirectory();
-        if(!folderExist){
-            folder.mkdir();
-        }
     }
 }
--- src/test/java/com/grimacegram/grimacegram/StaticResourcesTest.java
+++ src/test/java/com/grimacegram/grimacegram/StaticResourcesTest.java
@@ -1,10 +1,7 @@
 package com.grimacegram.grimacegram;
 
-import com.grimacegram.grimacegram.configuration.AppConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,28 +16,11 @@ import java.io.File;
 @ActiveProfiles("test")
 public class StaticResourcesTest {
 
-@Autowired
-    AppConfiguration appConfiguration;
-
     @Test
     public void checkStaticFolder_whenAppIsInitialized_uploadFolderMustExist(){
-        File uploadFolder = new File(appConfiguration.getUploadPath());
+        File uploadFolder = new File("upload-test");
         boolean uploadFolderExist = uploadFolder.exists() && uploadFolder.isDirectory();
         assertThat(uploadFolderExist).isTrue();
-    }
-    @Test
-    public void checkStaticFolder_whenAppIsInitialized_profileImageSubFolderMustExist(){
-        String profileImageFolderPath = appConfiguration.getFullProfileImagesPath();
-        File profileImageFolder = new File(profileImageFolderPath);
-        boolean profileImageFolderExist = profileImageFolder.exists() && profileImageFolder.isDirectory();
-        assertThat(profileImageFolderExist).isTrue();
-    }
-    @Test
-    public void checkStaticFolder_whenAppIsInitialized_attachmentsSubfolderMustExist(){
-        String attachmentsFolderPath = appConfiguration.getFullAttachmentsPath();
-        File attachmentFolder = new File(attachmentsFolderPath);
-        boolean attachmentsFolderExist = attachmentFolder.exists() && attachmentFolder.isDirectory();
-        assertThat(attachmentsFolderExist).isTrue();
     }
 
 }
