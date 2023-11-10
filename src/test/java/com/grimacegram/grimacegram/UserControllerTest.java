c17512fc4772019d2992e86c8bd99bddaf514444
asaaev <niaava1@gmail.com>
2023-11-10 14:08:15 +0200

    Add file upload implementation.

--- src/test/java/com/grimacegram/grimacegram/UserControllerTest.java
+++ src/test/java/com/grimacegram/grimacegram/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.grimacegram.grimacegram;
 
+import com.grimacegram.grimacegram.configuration.AppConfiguration;
 import com.grimacegram.grimacegram.error.ApiError;
 import com.grimacegram.grimacegram.model.User;
 import com.grimacegram.grimacegram.repository.UserRepository;
@@ -27,6 +28,7 @@ import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -52,6 +54,9 @@ public class UserControllerTest {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    AppConfiguration appConfiguration;
 
     @Before
     public void cleanDb(){
@@ -84,6 +89,12 @@ public class UserControllerTest {
     private void authenticate(String username){
         testRestTemplate.getRestTemplate().getInterceptors()
                 .add(new BasicAuthenticationInterceptor(username, "P4ssword"));
+    }
+    private String readFileToBase64(String fileName) throws IOException {
+        ClassPathResource imageResource = new ClassPathResource(fileName);
+        byte[] imageArr = FileUtils.readFileToByteArray(imageResource.getFile());
+        String imageString = Base64.getEncoder().encodeToString(imageArr);
+        return imageString;
     }
 
     @Test
@@ -438,25 +449,44 @@ public class UserControllerTest {
     public void putUser_whenValidRequestBodyWithSupportedImageFromAuthorizedUser_receiveUserVMWithRandomImageName() throws IOException {
         User user = userService.save(TestUtil.createValidUser("user1"));
         authenticate(user.getUsername());
-
-        ClassPathResource imageResource = new ClassPathResource("profile.jpg");
-
         UserUpdateVM updatedUser = createValidUserUpdateVM();
 
-        byte[] imageArr = FileUtils.readFileToByteArray(imageResource.getFile());
-        String imageString = Base64.getEncoder().encodeToString(imageArr);
+        String imageString = readFileToBase64("profile.jpg");
         updatedUser.setImage(imageString);
 
         HttpEntity<UserUpdateVM> requestEntity = new HttpEntity<>(updatedUser);
         ResponseEntity<UserVM> response = putUser(user.getUserId(), requestEntity, UserVM.class);
 
-        assertThat(response.getBody().getImage()).isNotEqualTo("profile-image.png");
+        assertThat(response.getBody().getImage()).isNotEqualTo("profile-image.jpg");
 
+    }
+    @Test
+    public void putUser_whenValidRequestBodyWithSupportedImageFromAuthorizedUser_imageIsStoredUnderProfileFolder() throws IOException {
+        User user = userService.save(TestUtil.createValidUser("user1"));
+        authenticate(user.getUsername());
+        UserUpdateVM updatedUser = createValidUserUpdateVM();
+
+        String imageString = readFileToBase64("profile.jpg");
+        updatedUser.setImage(imageString);
+
+        HttpEntity<UserUpdateVM> requestEntity = new HttpEntity<>(updatedUser);
+        ResponseEntity<UserVM> response = putUser(user.getUserId(), requestEntity, UserVM.class);
+
+        String storedImageName = response.getBody().getImage();
+        String profilePicturePath = appConfiguration.getFullProfileImagesPath() + "/" + storedImageName;
+        File storedImage = new File(profilePicturePath);
+
+        assertThat(storedImage.exists()).isTrue();
     }
 
     private UserUpdateVM createValidUserUpdateVM() {
         UserUpdateVM updatedUser = new UserUpdateVM();
         updatedUser.setDisplayName("newDisplayName");
         return updatedUser;
+    }
+    @After
+    public void cleanDirectory() throws IOException {
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullProfileImagesPath()));
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullAttachmentsPath()));
     }
 }
