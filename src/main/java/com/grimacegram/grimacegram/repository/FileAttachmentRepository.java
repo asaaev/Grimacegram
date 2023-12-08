0d0b545697e4e0e2452fb80ad80f322b4702a701
asaaev <niaava1@gmail.com>
2023-12-08 21:51:33 +0200

    adjust stale file query

--- src/main/java/com/grimacegram/grimacegram/repository/FileAttachmentRepository.java
+++ src/main/java/com/grimacegram/grimacegram/repository/FileAttachmentRepository.java
@@ -3,7 +3,10 @@ package com.grimacegram.grimacegram.repository;
 import com.grimacegram.grimacegram.shared.FileAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
 
-
+    List<FileAttachment> findByDateBeforeAndGrimaceIsNull(Date date);
 }
